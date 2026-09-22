87c5b401f8954b954e197314311388ddd2a5e572
jonathanescobarcnx <jonathan.escobar@concentrix.com>
2026-09-22 14:28:56 -0600

    add isCallValidation to complete task and log

--- plugin-flex-ts-template-v2/src/feature-library/personal-data-crm/flex-hooks/actions/CompleteTask.ts
+++ plugin-flex-ts-template-v2/src/feature-library/personal-data-crm/flex-hooks/actions/CompleteTask.ts
@@ -9,7 +9,8 @@ export const actionHook = function requirePersonalDataCrmForm(flex: typeof Flex)
   flex.Actions.addListener(`${actionEvent}${actionName}`, async (payload, abortFunction) => {
     const task = payload.task || (payload.sid ? flex.TaskHelper.getTaskByTaskSid(payload.sid) : undefined);
 
-    if (!task || task.attributes.personal_data_crm?.formSubmitted === true) {
+    if (!task || task.attributes.personal_data_crm?.formSubmitted === true || !flex.TaskHelper.isCallTask(task)) {
+      console.log('No task, form already submitted, or not a call task. Allowing CompleteTask action to proceed.', JSON.stringify(task));
       return;
     }
 
